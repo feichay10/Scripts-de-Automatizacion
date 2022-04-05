c937e1b5cba7d4d2c2a578c29d36994e98979c09
feichay10 <alu0101364544@ull.edu.es>
2022-04-05 09:14:11 +0100

    Actualizacion de los scripts

--- InstalacionNemoLinux.sh
+++ InstalacionNemoLinux.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-VIM="which vim"
-vacio=""
+opcion=
 
-sudo apt purge nautilus gnome-shell-extension-desktop-icons -y
+if [ "$(whoami)" == "root" ]; then
+  sudo apt purge nautilus gnome-shell-extension-desktop-icons -y
+  sudo apt install nemo -y
+  xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
+  settings set org.gnome.desktop.background show-desktop-icons false
+  gsettings set org.nemo.desktop show-desktop-icons true
+  echo "[Desktop Entry]" > /etc/xdg/autostart/nemo-autostart.desktop
+  echo "Type=Application" >> /etc/xdg/autostart/nemo-autostart.desktop
+  echo "Name=Files" >> /etc/xdg/autostart/nemo-autostart.desktop
+  echo "Exec=nemo-desktop" >> /etc/xdg/autostart/nemo-autostart.desktop
+  echo "OnlyShowIn=GNOME;Unity;" >> /etc/xdg/autostart/nemo-autostart.desktop
+  echo "X-Ubuntu-Gettext-Domain=nemo" >> /etc/xdg/autostart/nemo-autostart.desktop
 
-sudo apt install nemo -y
+  echo "Para que se aplique los cambios tiene que reiniciar el equipo [Y/n]: "
+  read opcion
 
-xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
-
-gsettings set org.gnome.desktop.background show-desktop-icons false
-
-gsettings set org.nemo.desktop show-desktop-icons true
-
-echo "Ahora va a editar un fichero y tendra que introducir el siguiente \"codigo\" (dentro de este script) para seguir con la instalacion: "
-echo "ENTER para continuar: "
-read vacio
-$vacio > /dev/null
-
-$VIM > /dev/null || sudo vim /etc/xdg/autostart/nemo-autostart.desktop
-
-# Copia este contenido en el fichero:
-# [Desktop Entry]
-# Type=Application
-# Name=Files
-# Exec=nemo-desktop
-# OnlyShowIn=GNOME;Unity;
-# X-Ubuntu-Gettext-Domain=nemo
-
-# Reinicia el PC
-# reboot
+  if [ "$opcion" == "y" ] || [ "$opcion" == "Y" ] ; then
+    reboot
+  elif [ "$opcion" == "n" ] || [ "$opcion" == "N" ]; then
+    echo "No se ha reiniciado el equipo"
+    exit 0
+  else 
+    echo "Opcion no valida"
+    exit 1
+  fi
+else
+  echo "Para ejecutar el script tienes que ser root"
+fi
--- VPNULL.sh
+++ VPNULL.sh
@@ -1,37 +1,45 @@
 #!/bin/bash
 
-###### Instalacion de la VPN en Linux #####
-# sudo apt-get -y update
-# sudo apt install -y network-manager-vpnc-gnome
-# sudo cat > /etc/vpnc/ull.conf << EOF
-# IPSec gateway vpn.ull.es
-# IPSec ID ULL
-# IPSec secret usu4r10s
-# EOF
-
+VPNC="which vpnc"
 opcion=
+etiqueta=0
 
-echo "Script para entrar a la VPN de la ULL
-      0) Para salir
-      1) Para conectarse a la VPN
-      2) Para desconectarse a la VPN
-      "
-echo -n "Introduzca una opcion: "
-read opcion
 
-case $opcion in
-  0) 
-    exit
-    ;;
-  1)
-    echo "Conectando con la VPN de la ULL..."
-    sudo vpnc ull.conf
-    ;;
-  2) 
-    echo "Desconectando de la VPN de la ULL..."
-    sudo vpnc-disconnect 
-    ;;
-  *)
-    echo "Opcion no valida. 0) Salir, 1) Para conectarse 2) Para desconectarse"
-esac
-#Para desconectarse de la conexión VPN: sudo vpnc-disconnect
+$VPNC > /dev/null || etiqueta=1
+
+if [ "$etiqueta" == "0" ]; then
+  echo "Script para entrar a la VPN de la ULL
+        0) Para salir
+        1) Para conectarse a la VPN
+        2) Para desconectarse a la VPN
+        "
+  echo -n "Introduzca una opcion: "
+  read opcion
+
+  case $opcion in
+    0) 
+      exit
+      ;;
+    1)
+      echo "Conectando con la VPN de la ULL..."
+      sudo vpnc ull.conf
+      ;;
+    2) 
+      echo "Desconectando de la VPN de la ULL..."
+      sudo vpnc-disconnect 
+      ;;
+    *)
+      echo "Opcion no valida. 0) Salir, 1) Para conectarse, 2) Para desconectarse"
+  esac
+else
+  if [ "$(whoami)" == "root" ]; then
+    apt-get -y update
+    apt install -y network-manager-vpnc-gnome
+    touch /etc/vpnc/ull.conf
+    echo "IPSec gateway vpn.ull.es" > /etc/vpnc/ull.conf
+    echo "IPSec ID ULL" >> /etc/vpnc/ull.conf
+    echo "IPSec secret usu4r10s" >> /etc/vpnc/ull.conf
+  else
+    echo "Para ejecutar el script tienes que ser root"
+  fi
+fi
